0e356a6b9323808cb25016094bf7e7cb75ac2ff7
Xidorn Quan <me@upsuper.org>
2016-12-25 11:52:06 +1100

    更新依赖

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate enum_set;
 extern crate getopts;
+#[macro_use]
+extern crate html5ever_atoms;
 extern crate kuchiki;
 extern crate selectors;
-#[macro_use] extern crate string_cache;
 
 mod classifier;
 mod data;
